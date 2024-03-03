501257b589b5f8f9e17efaf4d39e8f8f3f672358
rishabhdev444 <rishabhdev444@gmail.com>
2024-03-03 20:12:43 +0530

    viteconfig updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "chat-application",
+  base: "/chat-application",
   plugins: [react()],
   commonjsOptions: {
     transformMixedEsModules: true 
